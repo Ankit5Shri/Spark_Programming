ac61ebb408c243504407f50b7982aa0bdb7a078c
Ankit Shrivastava <33157304+Ankit5Shri@users.noreply.github.com>
2018-12-31 08:45:42 +0530

    Added comment

--- Spark+SQL,+DataFrames+and+Datasets+Guide+1.py
+++ Spark+SQL,+DataFrames+and+Datasets+Guide+1.py
@@ -1,63 +1,29 @@
-
-
-```python
 from pyspark import SparkContext
 sc = SparkContext()
-```
 
-
-```python
 from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
-```
 
-
-```python
 df= sqlContext.read.json('people.json')
-```
 
-
-```python
 type(df)
-```
 
+#pyspark.sql.dataframe.DataFrame
 
-
-
-    pyspark.sql.dataframe.DataFrame
-
-
-
-
-```python
 df.collect()
-```
 
+ #     [Row(age=None, name='Michael'),
+ #     Row(age=30, name='Andy'),
+ #     Row(age=19, name='Justin')]
 
-
-
-    [Row(age=None, name='Michael'),
-     Row(age=30, name='Andy'),
-     Row(age=19, name='Justin')]
-
-
-
-
-```python
 df.show()
-```
 
-    +----+-------+
-    | age|   name|
-    +----+-------+
-    |null|Michael|
-    |  30|   Andy|
-    |  19| Justin|
-    +----+-------+
+    # +----+-------+
+    # | age|   name|
+    # +----+-------+
+    # |null|Michael|
+    # |  30|   Andy|
+    # |  19| Justin|
+    # +----+-------+
     
 
-
-
-```python
-
-```
